3b8e1cdd97212edc1408fea5f719cba20906b0d6
arthan1011 <arthan1011@gmail.com>
2016-09-21 19:49:35 +0300

    security enabled

---
+++ blackjack/src/main/java/org/bigj/blackjack/config/BlackjackSecurityConfig.java
@@ -0,0 +1,29 @@
+package org.bigj.blackjack.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by arthan on 21.09.2016. | Project game-platform
+ */
+
+@EnableWebSecurity
+public class BlackjackSecurityConfig  extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user1").password("user").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/**").access("hasRole('USER')")
+            .and().formLogin()
+            .and().csrf().disable()
+        ;
+    }
+}
---
+++ blackjack/src/main/java/org/bigj/blackjack/config/BlackjackSecurityInitializer.java
@@ -0,0 +1,9 @@
+package org.bigj.blackjack.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by arthan on 21.09.2016. | Project game-platform
+ */
+public class BlackjackSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
